57cd2ea8edd59f28753e9d8525ac7100ce697e8e
GitDidrik <lindberg.didrik@gmail.com>
2023-05-08 15:29:02 -0700

    signup form

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,6 +1,35 @@
 const router = require('express').Router();
 const { User } = require('../../models');
 
+// CREATE new user
+router.post('/', async (req,res) => {
+  try{
+      const userData = await User.create(req.body)
+
+      req.session.save(() =>{
+          req.session.user_id = userData.id;
+          req.session.email = userData.email;
+          req.session.email = userData.username;
+          req.session.logged_in = true
+      })
+      res.status(200).json({ user: userData, message: 'You are now logged in!' });
+  }catch(err){
+      res.status(400).json(err)
+  }
+})
+
+//get all users
+router.get('/', async (req, res) => {
+  try {
+    const users = await User.findAll();
+    res.status(200).json(users);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: 'Server Error' });
+  }
+});
+
+
 router.post('/login', async (req, res) => {
     try {
       // Find the user who matches the posted e-mail address
